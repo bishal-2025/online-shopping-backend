651ea92c4cb5ddf6bb5a7f02cde4d540ba010df4
bishalawasthi <bishalawasthi2019@gmail.com>
2025-02-27 18:04:20 -0600

    Init project

---
+++ src/main/java/com/bishal/online_shopping_be/OnlineShoppingBeApplication.java
@@ -0,0 +1,13 @@
+package com.bishal.online_shopping_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bishal/online_shopping_be/OnlineShoppingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bishal.online_shopping_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
